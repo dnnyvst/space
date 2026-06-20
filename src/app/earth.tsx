57394bcaec2188c8462fae4cc2d383b81b02e33d
Danny Vasta <djv711@gmail.com>
2026-06-20 02:40:38 -0400

    add tilt

--- src/app/earth.tsx
+++ src/app/earth.tsx
@@ -1,25 +1,36 @@
 import { useRef } from "react";
+import * as THREE from "three";
 import { useFrame } from "@react-three/fiber";
 import { useTexture } from "@react-three/drei";
 
+const AXIAL_TILT = THREE.MathUtils.degToRad(23.44);
+
 export const Earth = () => {
+  const groupRef = useRef();
   const meshRef = useRef(null);
   const [dayMap, normalMap] = useTexture([
     "/textures/earth_daymap.jpg",
     "/textures/earth_normal_map.jpg",
   ]);
 
-  useFrame(({ clock }) => {
+  useFrame((_, delta) => {
     if (meshRef.current) {
-      meshRef.current.rotation.y = clock.elapsedTime / 4;
-      meshRef.current.rotation.z = clock.elapsedTime / 8;
+      meshRef.current.rotation.y += delta * 0.1;
     }
   });
 
   return (
-    <mesh ref={meshRef}>
-      <sphereGeometry args={[2, 64, 64]} />
-      <meshStandardMaterial map={dayMap} normalMap={normalMap} />
-    </mesh>
+    <group ref={groupRef} rotation={[0, 0, AXIAL_TILT]}>
+      <mesh ref={meshRef}>
+        <sphereGeometry args={[2, 64, 64]} />
+        <meshStandardMaterial
+          map={dayMap}
+          normalMap={normalMap}
+          normalScale={[0.6, 0.6]}
+          roughness={0.9}
+          metalness={0}
+        />
+      </mesh>
+    </group>
   );
 };
--- src/app/main-canvas.tsx
+++ src/app/main-canvas.tsx
@@ -9,7 +9,7 @@ export const MainCanvas: FC = () => {
     <div className="border-1 border-gray-800 rounded-lg w-1/2 h-3/4">
       <Canvas>
         <ambientLight intensity={0.1} />
-        <directionalLight color="white" position={[2, 2, 5]} />
+        <directionalLight position={[2, 2, 5]} />
         <Earth />
       </Canvas>
     </div>
