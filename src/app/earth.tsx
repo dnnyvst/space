c974bd028a3dedf6ba09564f47645ca1fdc02ffb
Danny Vasta <djv711@gmail.com>
2026-06-20 02:54:20 -0400

    add clouds and sun

--- src/app/earth.tsx
+++ src/app/earth.tsx
@@ -6,29 +6,40 @@ import { useTexture } from "@react-three/drei";
 const AXIAL_TILT = THREE.MathUtils.degToRad(23.44);
 
 export const Earth = () => {
-  const groupRef = useRef();
-  const meshRef = useRef(null);
-  const [dayMap, normalMap] = useTexture([
+  const groupRef = useRef<THREE.Group>(null);
+  const earthRef = useRef<THREE.Mesh>(null);
+  const cloudsRef = useRef<THREE.Mesh>(null);
+
+  const [dayMap, normalMap, cloudsMap] = useTexture([
     "/textures/earth_daymap.jpg",
     "/textures/earth_normal_map.jpg",
+    "/textures/earth_clouds.jpg",
   ]);
 
-  useFrame((_, delta) => {
-    if (meshRef.current) {
-      meshRef.current.rotation.y += delta * 0.1;
-    }
+  useFrame((state, delta) => {
+    earthRef.current!.rotation.y += delta * 0.1;
+    cloudsRef.current!.rotation.y += delta * 0.13;
+
+    groupRef.current!.rotation.z =
+      AXIAL_TILT + Math.sin(state.clock.elapsedTime * 0.1) * 0.002;
   });
 
   return (
-    <group ref={groupRef} rotation={[0, 0, AXIAL_TILT]}>
-      <mesh ref={meshRef}>
+    <group ref={groupRef} rotation={[AXIAL_TILT, 0, 0]}>
+      {/* Earth */}
+      <mesh ref={earthRef}>
         <sphereGeometry args={[2, 64, 64]} />
+        <meshStandardMaterial map={dayMap} normalMap={normalMap} />
+      </mesh>
+
+      {/* Clouds */}
+      <mesh ref={cloudsRef}>
+        <sphereGeometry args={[2.03, 64, 64]} />
         <meshStandardMaterial
-          map={dayMap}
-          normalMap={normalMap}
-          normalScale={[0.6, 0.6]}
-          roughness={0.9}
-          metalness={0}
+          map={cloudsMap}
+          transparent
+          opacity={0.4}
+          depthWrite={false}
         />
       </mesh>
     </group>
--- src/app/main-canvas.tsx
+++ src/app/main-canvas.tsx
@@ -9,7 +9,12 @@ export const MainCanvas: FC = () => {
     <div className="border-1 border-gray-800 rounded-lg w-1/2 h-3/4">
       <Canvas>
         <ambientLight intensity={0.1} />
-        <directionalLight position={[2, 2, 5]} />
+        <directionalLight
+          position={[5, 5, 5]}
+          intensity={2}
+          castShadow
+          color="#fff1d6"
+        />
         <Earth />
       </Canvas>
     </div>
