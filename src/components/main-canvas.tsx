21d55211b027f91baed99d2a735389f75df4c16f
Danny Vasta <djv711@gmail.com>
2026-06-20 16:23:29 -0400

    ui update part one

--- src/components/main-canvas.tsx
+++ src/components/main-canvas.tsx
@@ -11,41 +11,52 @@ export const MainCanvas: FC = () => {
   const selectedPlanet = PLANET_CONFIG[selectedPlanetId];
 
   return (
-    <div className="h-screen flex flex-col items-center font-mono">
-      <div className="flex gap-8 mt-10 bg-black w-min border-1 border-white/30 py-2 px-4 rounded-lg">
-        {Object.values(PLANET_CONFIG).map(({ id, name }) => (
-          <button
-            key={id}
-            className={`cursor-pointer transition-all duration-300 ease-out ${
-              id === selectedPlanetId
-                ? "underline underline-offset-4 decoration-2 opacity-100"
-                : "opacity-30 hover:opacity-70"
-            }`}
-            onClick={() => setSelectedPlanetId(id)}
-          >
-            {name}
-          </button>
-        ))}
+    <div className="h-screen relative font-mono">
+      {/* UI OVERLAY*/}
+      <div className="absolute top-6 left-1/2 -translate-x-1/2 flex flex-col gap-4 z-10">
+        {/* Planet select */}
+        <div className="flex gap-6 bg-black w-min border-1 border-white/30 py-2 px-4 rounded-lg">
+          {Object.values(PLANET_CONFIG).map(({ id, name }) => (
+            <button
+              key={id}
+              className={`cursor-pointer transition-all duration-300 ease-out ${
+                id === selectedPlanetId
+                  ? "underline underline-offset-4 decoration-2 opacity-100"
+                  : "opacity-30 hover:opacity-70"
+              }`}
+              onClick={() => setSelectedPlanetId(id)}
+            >
+              {name}
+            </button>
+          ))}
+        </div>
+
+        {/* Toggles (placeholder) */}
+        <div className="flex bg-black w-min border-1 border-white/30 py-2 px-4 rounded-lg">
+          test
+        </div>
       </div>
-      <div className="flex-1 w-full relative">
-        <Canvas className="w-full h-full">
-          <ambientLight intensity={selectedPlanetId === "earth" ? 0.2 : 0.1} />
-          <SunLight />
-          <Planet
-            axialTilt={selectedPlanet.tilt}
-            retrograde={selectedPlanet.retrograde}
-            textures={selectedPlanet.textures}
-          />
-        </Canvas>
-        {selectedPlanetId === "saturn" && (
-          <div className="absolute inset-0 flex justify-center items-center h-3/4 text-black">
-            under construction.
-            <br />
-            <br />
-            rings are too hard.
-          </div>
-        )}
-      </div>
+
+      {/* CANVAS (full screen, never moves) */}
+      <Canvas className="w-full h-full">
+        <ambientLight intensity={selectedPlanetId === "earth" ? 0.2 : 0.1} />
+        <SunLight />
+        <Planet
+          axialTilt={selectedPlanet.tilt}
+          retrograde={selectedPlanet.retrograde}
+          textures={selectedPlanet.textures}
+        />
+      </Canvas>
+
+      {/* Saturn overlay */}
+      {selectedPlanetId === "saturn" && (
+        <div className="absolute inset-0 flex justify-center items-center text-black">
+          under construction.
+          <br />
+          <br />
+          rings are too hard.
+        </div>
+      )}
     </div>
   );
 };
