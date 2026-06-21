375ab15177a36434bd12bb482c83600abc942e27
Danny Vasta <djv711@gmail.com>
2026-06-20 22:30:11 -0400

    and up again

--- src/components/main-canvas.tsx
+++ src/components/main-canvas.tsx
@@ -116,7 +116,7 @@ export const MainCanvas: FC = () => {
           /> */}
           <Bloom
             intensity={1}
-            luminanceThreshold={0.15}
+            luminanceThreshold={0.2}
             luminanceSmoothing={0.6}
           />
           <Vignette eskil={false} offset={0.1} darkness={0.7} />
