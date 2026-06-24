a258ef0c89f7798c43ce0d0156974e9309fa9601
Danny Vasta <djv711@gmail.com>
2026-06-23 21:42:15 -0400

    progress

--- src/components/ui/ui-overlay.tsx
+++ src/components/ui/ui-overlay.tsx
@@ -45,67 +45,67 @@ export const UIOverlay: FC = () => {
   );
 
   return (
-    <div
-      className={`items-center flex flex-col-reverse justify-between gap-4 h-screen absolute py-4 px-16 z-10 w-full lg:items-start lg:px-4 lg:flex-row lg:justify-center ${isMobile && "pb-14"}`}
-    >
+    <div className="flex justify-center h-screen absolute py-4 px-12 z-10 w-full sm:px-8 lg:px-4">
       {/* controls */}
-      <div className="flex gap-2 justify-around lg:flex-col lg:justify-start">
-        {/* sliders */}
-        <div className="flex flex-col gap-2 bg-card/0 whitespace-nowrap border border-text/30 py-2 px-4 rounded-lg z-10 h-min">
-          <span className="flex flex-col">
-            <span className="text-center">fov: {fov}&deg;</span>
-            <FOVSlider />
-          </span>
-          <span className="flex flex-col">
-            <span className={`text-center ${orbitMode && "opacity-20"}`}>
-              zoom
+      <div className="items-center flex flex-col-reverse justify-end gap-4 w-max lg:w-auto lg:items-start lg:flex-row lg:justify-center">
+        <div className="flex gap-2 justify-around lg:flex-col lg:justify-start self-start">
+          {/* sliders */}
+          <div className="flex flex-col gap-2 bg-card/0 whitespace-nowrap border border-text/30 py-2 px-4 rounded-lg z-10 h-min">
+            <span className="flex flex-col">
+              <span className="text-center">fov: {fov}&deg;</span>
+              <FOVSlider />
             </span>
-            <CameraZoomSlider disabled={orbitMode} />
-          </span>
-        </div>
-        {/* toggles */}
-        <div className="bg-card/0 whitespace-nowrap border border-text/30 py-2 px-4 rounded-lg z-10 h-min">
-          <ul>
-            <ListItem
-              selected={orbitMode === true}
-              onClick={() => setOrbitMode((orbitMode) => !orbitMode)}
-              text={`orbit ${!isMobile ? "cam" : ""}`}
-            />
-            {Object.keys(toggleTextures).map((property) => (
+            <span className="flex flex-col">
+              <span className={`text-center ${orbitMode && "opacity-20"}`}>
+                zoom
+              </span>
+              <CameraZoomSlider disabled={orbitMode} />
+            </span>
+          </div>
+          {/* toggles */}
+          <div className="bg-card/0 whitespace-nowrap border border-text/30 py-2 px-4 rounded-lg z-10 h-min">
+            <ul>
               <ListItem
-                key={property}
-                selected={selectedProperties.has(property)}
-                onClick={() =>
-                  setSelectedProperties((prev) => {
-                    if (prev.has(property)) {
-                      return new Set([...prev].filter((p) => p !== property));
-                    }
-                    return new Set([...prev, property]);
-                  })
-                }
-                text={property}
+                selected={orbitMode === true}
+                onClick={() => setOrbitMode((orbitMode) => !orbitMode)}
+                text={`orbit ${!isMobile ? "cam" : ""}`}
               />
-            ))}
-          </ul>
+              {Object.keys(toggleTextures).map((property) => (
+                <ListItem
+                  key={property}
+                  selected={selectedProperties.has(property)}
+                  onClick={() =>
+                    setSelectedProperties((prev) => {
+                      if (prev.has(property)) {
+                        return new Set([...prev].filter((p) => p !== property));
+                      }
+                      return new Set([...prev, property]);
+                    })
+                  }
+                  text={property}
+                />
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
-      {/* celestial body select */}
-      <div
-        className={`flex flex-wrap gap-3 bg-card/0 border border-text/30 py-2 px-4 rounded-lg h-min lg:w-min lg:flex-nowrap lg:gap-6 ${orbitMode && "invisible opacity-0"} `}
-      >
-        {Object.values(CELESTIAL_BODY_CONFIG).map(({ id, name }) => (
-          <button
-            key={id}
-            className={`block cursor-pointer transition-all duration-300 ease-out ${
-              id === selectedCelestialBody.id
-                ? "underline underline-offset-4 decoration-2 opacity-100"
-                : "opacity-30 hover:opacity-70"
-            }`}
-            onClick={() => setSelectedCelestialBodyId(id)}
-          >
-            {name}
-          </button>
-        ))}
+        {/* celestial body select */}
+        <div
+          className={`flex flex-wrap gap-3 bg-card/0 border border-text/30 py-2 px-4 rounded-lg h-min lg:w-min lg:flex-nowrap lg:gap-6 ${orbitMode && "invisible opacity-0"} `}
+        >
+          {Object.values(CELESTIAL_BODY_CONFIG).map(({ id, name }) => (
+            <button
+              key={id}
+              className={`block cursor-pointer transition-all duration-300 ease-out ${
+                id === selectedCelestialBody.id
+                  ? "underline underline-offset-4 decoration-2 opacity-100"
+                  : "opacity-30 hover:opacity-70"
+              }`}
+              onClick={() => setSelectedCelestialBodyId(id)}
+            >
+              {name}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
