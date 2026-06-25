e12fbfd4ccf8ef4f702f32bf750c946a58a43ac7
Danny Vasta <djv711@gmail.com>
2026-06-24 20:01:27 -0400

    preload font

--- src/components/main-page.tsx
+++ src/components/main-page.tsx
@@ -1,12 +1,25 @@
 "use client";
 
+import { useEffect } from "react";
 import { MainCanvas } from "@/components";
 import { AppContextProvider, CameraContextProvider } from "@/hooks";
+import { preloadFont } from "troika-three-text";
 
-export const MainPage = () => (
-  <AppContextProvider>
-    <CameraContextProvider>
-      <MainCanvas />
-    </CameraContextProvider>
-  </AppContextProvider>
-);
+export const MainPage = () => {
+  useEffect(() => {
+    preloadFont(
+      {
+        font: "/fonts/GeistMono-Regular.ttf",
+      },
+      () => {},
+    );
+  }, []);
+
+  return (
+    <AppContextProvider>
+      <CameraContextProvider>
+        <MainCanvas />
+      </CameraContextProvider>
+    </AppContextProvider>
+  );
+};
