c1286fc0238d0d49f511fe213c07e2bff43b3697
Danny Vasta <djv711@gmail.com>
2026-06-26 02:31:18 -0400

    fix follow cam stutter

--- src/cameras/follow-camera.tsx
+++ src/cameras/follow-camera.tsx
@@ -1,4 +1,4 @@
-import { type FC } from "react";
+import { useEffect, useRef, type FC } from "react";
 import * as THREE from "three";
 import { useFrame, useThree } from "@react-three/fiber";
 import { useCameraContext } from "@/hooks";
@@ -7,22 +7,27 @@ interface FollowCameraProps {
   enabled?: boolean;
 }
 
+const target = new THREE.Vector3();
+const upOffset = new THREE.Vector3(0, 0.25, 0);
+const offset = new THREE.Vector3();
+const direction = new THREE.Vector3();
+const newCameraPosition = new THREE.Vector3();
+
 export const FollowCamera: FC<FollowCameraProps> = ({ enabled = false }) => {
   const { camera, scene } = useThree();
 
   const { followName } = useCameraContext();
 
-  const followObject = scene.getObjectByName(followName ?? "");
+  const followRef = useRef<THREE.Object3D | null | undefined>(null);
 
-  const offset = new THREE.Vector3();
-  const direction = new THREE.Vector3();
-  const newCameraPosition = new THREE.Vector3();
+  useEffect(() => {
+    followRef.current = followName ? scene.getObjectByName(followName) : null;
+  }, [followName, scene]);
 
   useFrame((_, delta) => {
-    if (!enabled || !followObject) return;
+    if (!enabled || !followRef?.current) return;
 
-    const target = new THREE.Vector3();
-    followObject.getWorldPosition(target);
+    followRef.current.getWorldPosition(target);
 
     // todo - use parent center via
     // target.clone().sub(planet.position).normalize()
@@ -34,7 +39,7 @@ export const FollowCamera: FC<FollowCameraProps> = ({ enabled = false }) => {
     offset
       .copy(direction)
       .multiplyScalar(followName === "moon" ? 1 : 0.5)
-      .add(new THREE.Vector3(0, 0.25, 0));
+      .add(upOffset);
 
     newCameraPosition.copy(target).add(offset);
 
@@ -44,7 +49,14 @@ export const FollowCamera: FC<FollowCameraProps> = ({ enabled = false }) => {
       THREE.MathUtils.damp(camera.position.z, newCameraPosition.z, 3, delta),
     );
 
-    camera.lookAt(target);
+    // camera.lookAt(target);
+    const lookAtMatrix = new THREE.Matrix4();
+    const targetQuat = new THREE.Quaternion();
+
+    lookAtMatrix.lookAt(camera.position, target, camera.up);
+    targetQuat.setFromRotationMatrix(lookAtMatrix);
+
+    camera.quaternion.slerp(targetQuat, 1 - Math.exp(-6 * delta));
   });
 
   return null;
